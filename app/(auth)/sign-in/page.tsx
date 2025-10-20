b7a0edc6539d1c1cc0ac4fac9a00721e4d4ecf28
waidzk <waidzk30@gmail.com>
2025-10-20 14:35:54 +0700

    setup app, file and folder structure, routing and auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { AuthForm } from '@/components/AuthForm'
+
+const page = () => {
+  return (
+    <AuthForm type="sign-in" />
+  )
+}
+
+export default page
---
+++ components/FormField.tsx
@@ -0,0 +1,46 @@
+import { Input } from "@/components/ui/input";
+import {
+  FormControl,
+  FormDescription,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Controller, FieldValues, Path, Control } from "react-hook-form";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{label}</FormLabel>
+        <FormControl>
+          <Input
+            type={type}
+            className="input"
+            placeholder={placeholder}
+            {...field}
+          />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormField;
